f172548cb05efe5c45297259ec29a77e27705688
Simon Vrachliotis <simon@simonswiss.com>
2024-03-18 08:49:15 +1100

    initial commit

---
+++ db/seed.ts
@@ -0,0 +1,6 @@
+import { db } from 'astro:db';
+
+export default async function seed() {
+	// Seed local development data.
+	// See https://docs.astro.build/en/guides/astro-db/#seed-your-database
+}
