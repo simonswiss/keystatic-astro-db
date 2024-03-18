9812d6b83e22b3e47f622cea999eb000b4c6f932
Simon Vrachliotis <simon@simonswiss.com>
2024-03-18 21:01:52 +1100

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import db from "@astrojs/db"
-import { defineConfig } from "astro/config"
+import db from "@astrojs/db";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import keystatic from "@keystatic/astro";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react"
-import mdx from "@astrojs/mdx"
-import keystatic from "@keystatic/astro"
-import tailwindcss from "@tailwindcss/vite"
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), react(), mdx(), keystatic()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
   output: "hybrid",
-})
+  adapter: vercel()
+});
